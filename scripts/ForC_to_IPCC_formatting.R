d7f463eb3bd56f167d7b696e31eab44d4e1e62a2
Valentine Herrmann <herrmannv@si.edu>
2021-04-01 15:08:03 -0400

    make script empty 1-to-review folder before saving
    this is in case there is a different number of records.  If we don't do that, file name would be different and we would have duplicates.

--- scripts/ForC_to_IPCC_formatting.R
+++ scripts/ForC_to_IPCC_formatting.R
@@ -511,6 +511,8 @@ EFDB <- data.frame("EF ID" = "",
 
 # save one csv file per citation.ID into data/1-to-review
 
+## first delete any file so that if slightly different number od records, still ovewrites.
+file.remove(list.files("data/1-to-review/", pattern = ".csv"))
 
 for(c_id in   c("Archibald_2009_doiv",
                 "Chave_2008_aepa",
