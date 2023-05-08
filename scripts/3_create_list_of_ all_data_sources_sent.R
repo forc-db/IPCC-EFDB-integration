c5d87ef60d7566fec3f60eb1c75a2789d1468203
Herrmann <HerrmannV@SI.EDU>
2023-05-08 15:28:19 -0400

    Rerun after latest fix + group variables under pretty name #43 #44
    + fix typo in object name

--- scripts/3_create_list_of_ all_data_sources_sent.R
+++ scripts/3_create_list_of_ all_data_sources_sent.R
@@ -24,7 +24,7 @@ trace_of_measurement_IDs <- read.csv("data/3-EFDB-forms-ready/trace_of_measureme
 
 ## list of files sent
 list_files_sent <- list.files("data/4-transferred-to-EFDB/", pattern = ".txt", full.names = T)
-
+list_files_sent <- list_files_sent[list_files_sent!= "data/4-transferred-to-EFDB/notes_batch_6.txt"]
 
 # Create the table with all we need about the data we submitted ####
 summary_table <- NULL
@@ -67,66 +67,82 @@ ForC_simplified <- ForC_simplified[ForC_simplified$variable.name %in% gsub("_OM|
 
 
 ## simplify variable name
+variables_in_order <- read.csv("doc/manuscript/figures_tables/C_variables_order_template.csv")
+
 MEASUREMENTS$variable.name_simply <- gsub("_OM|_C", "", MEASUREMENTS$variable.name)
 
 ForC_simplified$variable.name_simply <- gsub("_OM|_C", "", ForC_simplified$variable.name)
 
+all(setdiff(variables_in_order$variable.name, MEASUREMENTS$variable.name_simply) %in% c("delta.biomass", "woody.mortality", "delta.biomass_root", "delta.biomass_root_coarse", 
+                                                                                         "delta.biomass_root_fine", "woody.mortality_root", "delta.deadwood", 
+                                                                                         "delta.deadwood_standing", "delta.deadwood_down", "R_het_deadwood", 
+                                                                                         "delta.litter", "delta.total.ecosystem_2", "delta.soil", "R_het_soil"
+))  # should be TRUE (all the ones that are not in n_in_ForC but are in C_variables_order_template.csv should be in the list pasted here)
+
+setdiff(MEASUREMENTS$variable.name_simply, variables_in_order$variable.name) # should be empty
+
+
+## combine variables that need to be grouped
+MEASUREMENTS$variable_pretty <- variables_in_order$variable[match(MEASUREMENTS$variable.name_simply, variables_in_order$variable.name)]
+
+ForC_simplified$variable_pretty <- variables_in_order$variable[match(ForC_simplified$variable.name_simply, variables_in_order$variable.name)]
+
+variables_in_order$variable.name <- NULL
+variables_in_order <- unique(variables_in_order)
 
 # count n records of various stages
-n_in_ForC <- as.data.frame(table(MEASUREMENTS$variable.name_simply), responseName = "n_in_ForC")
+n_in_ForC <- as.data.frame(table(MEASUREMENTS$variable_pretty), responseName = "n_in_ForC")
 
-n_independent <- as.data.frame(table(ForC_simplified$variable.name_simply[ForC_simplified$suspected.duplicate == 0]), responseName = "n_independent_records_in_ForC")
+n_independent <- as.data.frame(table(ForC_simplified$variable_pretty[ForC_simplified$suspected.duplicate == 0]), responseName = "n_independent_records_in_ForC")
 
-n_reviewed <- as.data.frame(table(ForC_simplified$variable.name_simply[ForC_simplified$citation.ID %in% CITATIONS$citation.ID[CITATIONS$EFDB.ready == 1]]), responseName = "n_reviewed")
+n_reviewed <- as.data.frame(table(ForC_simplified$variable_pretty[ForC_simplified$citation.ID %in% CITATIONS$citation.ID[CITATIONS$EFDB.ready == 1]]), responseName = "n_reviewed")
 
-n_sent_to_EFDB <- as.data.frame(table(MEASUREMENTS$variable.name_simply[MEASUREMENTS$measurement.ID %in% trace_of_measurement_IDs$measurement.ID]), responseName = "n_sent_to_EFDB")
-
-# combine all ###
-varibales_in_order <- read.csv("doc/manuscript/figures_tables/C_variables_order_template.csv")
-
-setdiff(varibales_in_order$variable.name, gsub("_OM|_C", "", VARIABLES$variable.name)) # should be empty
+n_sent_to_EFDB <- as.data.frame(table(MEASUREMENTS$variable_pretty[MEASUREMENTS$measurement.ID %in% trace_of_measurement_IDs$measurement.ID]), responseName = "n_sent_to_EFDB")
 
 
 
-varibales_in_order$n_in_ForC <- n_in_ForC$n_in_ForC[match(varibales_in_order$variable.name, n_in_ForC$Var1)]
-varibales_in_order$n_independent_records_in_ForC <- n_independent$n_independent_records_in_ForC[match(varibales_in_order$variable.name, n_independent$Var1)]
-varibales_in_order$n_reviewed <- n_reviewed$n_reviewed[match(varibales_in_order$variable.name, n_reviewed$Var1)]
-varibales_in_order$n_sent_to_EFDB <- n_sent_to_EFDB$n_sent_to_EFDB[match(varibales_in_order$variable.name, n_sent_to_EFDB$Var1)]
+# add the values to variables_in_order
+
+variables_in_order$n_in_ForC <- n_in_ForC$n_in_ForC[match(variables_in_order$variable, n_in_ForC$Var1)]
+variables_in_order$n_independent_records_in_ForC <- n_independent$n_independent_records_in_ForC[match(variables_in_order$variable, n_independent$Var1)]
+variables_in_order$n_reviewed <- n_reviewed$n_reviewed[match(variables_in_order$variable, n_reviewed$Var1)]
+variables_in_order$n_sent_to_EFDB <- n_sent_to_EFDB$n_sent_to_EFDB[match(variables_in_order$variable, n_sent_to_EFDB$Var1)]
 
 
-varibales_in_order$variable.name <- NULL
+
 
 # replace NA by 0
-varibales_in_order[is.na(varibales_in_order)] <- 0
+variables_in_order[is.na(variables_in_order)] <- 0
+
 
 # add a column that will indicate if need to be bold in table
-varibales_in_order <- cbind(bold = FALSE, varibales_in_order)
+variables_in_order <- cbind(bold = FALSE, variables_in_order)
 
 # add empty row for headers
-rows_to_add <- varibales_in_order[!duplicated(varibales_in_order$pool), ]
+rows_to_add <- variables_in_order[!duplicated(variables_in_order$pool), ]
 rows_to_add[1] <- TRUE
 rows_to_add[3] <- rows_to_add[2]
 rows_to_add[-c(1:3)] <- NA
 
-varibales_in_order$pool <- factor(varibales_in_order$pool, levels = unique(varibales_in_order$pool))
+variables_in_order$pool <- factor(variables_in_order$pool, levels = unique(variables_in_order$pool))
 
-varibales_in_order <- do.call(rbind, by(varibales_in_order, varibales_in_order$pool, function(x) rbind(rows_to_add[rows_to_add$pool %in% x$pool[1],], x)), )
+variables_in_order <- do.call(rbind, by(variables_in_order, variables_in_order$pool, function(x) rbind(rows_to_add[rows_to_add$pool %in% x$pool[1],], x)), )
 
 # remove pool column
-varibales_in_order$pool <- NULL
+variables_in_order$pool <- NULL
 
 
 # add a row for the total
 
-varibales_in_order <- rbind(varibales_in_order, c(TRUE, "TOTAL", t(colSums(varibales_in_order[, -c(1,2)], na.rm = T))))
+variables_in_order <- rbind(variables_in_order, c(TRUE, "TOTAL", t(colSums(variables_in_order[, -c(1,2)], na.rm = T))))
 
 
-colnames(varibales_in_order) <- gsub("_", " ", colnames(varibales_in_order))
+colnames(variables_in_order) <- gsub("_", " ", colnames(variables_in_order))
 
 
 
 
-write.csv(varibales_in_order, "doc/manuscript/figures_tables/C_variables.csv", row.names = F)
+write.csv(variables_in_order, "doc/manuscript/figures_tables/C_variables.csv", row.names = F)
 
 
 
